ce80f133c557e4263204967bce833ebcbf0f5148
Liang Niu <niuliang3@gmail.com>
2017-04-18 16:47:21 -0400

    sync to 6

--- hellotf.py
+++ hellotf.py
@@ -3,3 +3,15 @@ import tensorflow as tf
 node1 = tf.constant(3.0, tf.float32)
 node2 = tf.constant(4.0) # also tf.float32 implicitly
 print(node1, node2)
+sess = tf.Session()
+print(sess.run([node1, node2]))
+
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b  # + provides a shortcut for tf.add(a, b)
+print(sess.run(adder_node, {a: 3, b:4.5}))
+print(sess.run(adder_node, {a: [1,3], b: [2, 4]}))
+
+add_and_triple = adder_node * 3.
+print(sess.run(add_and_triple, {a: 3, b:4.5}))
+
